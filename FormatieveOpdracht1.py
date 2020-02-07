05e27183d4513004b2bc63d69e59208fe0559ca4
WijnandVanDijk <60653370+WijnandVanDijk@users.noreply.github.com>
2020-02-07 17:01:33 +0100

    2

---
+++ FormatieveOpdracht1.py
@@ -0,0 +1,14 @@
+n = int(input('Hoe groot?'))
+asterisk = '*'
+
+for i in range(n):
+    for j in range(0, i+1):
+        print(asterisk, end="")
+    print()
+
+for i in range(n):
+    for j in range(n-1, i, -1):
+        print(asterisk, end="")
+    print()
+
+# bron: https://pynative.com/print-pattern-python-examples/
---
+++ FormatieveOpdracht2.py
@@ -0,0 +1,131 @@
+string1 = input('Geef de eerste zin:')
+string2 = input('Geef de tweede zin:')
+
+if string1[0] == string2[0]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 0')
+
+if string1[1] == string2[1]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 1')
+if string1[2] == string2[2]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 2')
+
+if string1[3] == string2[3]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 3')
+
+if string1[4] == string2[4]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 4')
+
+if string1[5] == string2[5]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 5')
+
+if string1[6] == string2[6]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 6')
+
+if string1[7] == string2[7]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 7')
+
+if string1[8] == string2[8]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 8')
+
+if string1[9] == string2[9]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 9')
+
+if string1[10] == string2[10]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 10')
+
+if string1[11] == string2[11]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 11')
+
+if string1[12] == string2[12]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 12')
+
+if string1[13] == string2[13]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 13')
+
+if string1[14] == string2[14]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 14')
+
+if string1[14] == string2[14]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 15')
+
+if string1[14] == string2[14]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 16')
+
+if string1[14] == string2[14]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 17')
+
+if string1[14] == string2[14]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 18')
+
+if string1[14] == string2[14]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 19')
+
+if string1[14] == string2[14]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 20')
+
+if string1[14] == string2[14]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 21')
+
+if string1[14] == string2[14]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 22')
+
+if string1[14] == string2[14]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 23')
+
+if string1[14] == string2[14]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 24')
+
+if string1[14] == string2[14]:
+    print()
+else:
+    print('Het eerste verschil zit op index: 25')
