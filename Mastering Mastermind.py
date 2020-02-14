98fd80eb7b5eb0255654d35cfba9abc9df3affe2
WijnandVanDijk <60653370+WijnandVanDijk@users.noreply.github.com>
2020-02-14 14:35:52 +0100

    Update Mastering Mastermind.py

--- Mastering Mastermind.py
+++ Mastering Mastermind.py
@@ -1,3 +1,15 @@
-import random
+zwart = 0
+wit = 0
+def geeffeedback(zwart, wit):
+    code = input('Geef de code: ')
+    gok = ('a', 'f', 'd', 'b')
+    for i in range(len(code)):
+        if gok[i] == code[i]:
+            zwart += 1
 
-num = random.randrange(1000, 10000)
+    for i in range(len(code)):
+        if gok[i] != code[i] and gok[i] in code:
+            wit += 1
+
+    feedback = [zwart, wit]
+    print(feedback)
