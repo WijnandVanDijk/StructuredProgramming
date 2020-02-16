98d9d4251679165a81cc42cec590c6c402764515
WijnandVanDijk <60653370+WijnandVanDijk@users.noreply.github.com>
2020-02-16 18:57:33 +0100

    Update Mastering mastermind.py

--- Mastering mastermind.py
+++ Mastering mastermind.py
@@ -1,11 +1,21 @@
 import itertools
+import random
 zwart = 0
 wit = 0
+
+mogelijkheden = list(itertools.product(['a', 'b', 'c', 'd', 'e', 'f'], repeat=4))
+# bron voor mogelijkheden: https://docs.python.org/3/library/itertools.html
+
+
+def randomgok():
+    global gok
+    gok = random.choice(mogelijkheden)
+    print(gok)
+    geeffeedback(zwart, wit)
 
 
 def geeffeedback(zwart, wit):
     code = input('Geef de code: ')
-    gok = ('a', 'f', 'd', 'b')
     for i in range(len(code)):
         if gok[i] == code[i]:
             zwart += 1
@@ -14,11 +24,10 @@ def geeffeedback(zwart, wit):
         if gok[i] != code[i] and gok[i] in code:
             wit += 1
 
+    global feedback
     feedback = [zwart, wit]
     print(feedback)
 
 
-def mogelijkheden():
-    mogelijk = tuple(itertools.product(['a', 'b', 'c', 'd', 'e', 'f'], repeat=4))
-    print(mogelijk)
-#bron voor mogelijkheden: https://docs.python.org/3/library/itertools.html
+def gok2():
+
